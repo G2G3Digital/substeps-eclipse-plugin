f7002b9498e5ff7aae0005432a66d05ceba0f444
Stu Forbes <mr.stu.forbes@gmail.com>
2012-06-15 11:55:29 +0100

    Updating testframework to work in maven using new substeps-core

--- com.technophobia.substeps.editor.tests.acceptance/src/main/java/com/technophobia/substeps/editor/SimpleTest.java
+++ com.technophobia.substeps.editor.tests.acceptance/src/main/java/com/technophobia/substeps/editor/SimpleTest.java
@@ -6,12 +6,10 @@ import com.technophobia.substeps.editor.steps.BasicEditorSteps;
 import com.technophobia.substeps.editor.steps.ContentFormattingSteps;
 import com.technophobia.substeps.editor.steps.SWTBotInitialiser;
 import com.technophobia.substeps.editor.steps.SimpleSteps;
-import com.technophobia.substeps.runner.JunitFeatureRunner.BeforeAndAfterProcessors;
-import com.technophobia.substeps.runner.JunitFeatureRunner.FeatureFiles;
+import com.technophobia.substeps.runner.JunitFeatureRunner.SubStepsConfiguration;
 
 @RunWith(SubStepsSWTBotJunitClassRunner.class)
-@FeatureFiles(featureFile = "features/sample.feature", stepImplementations = {
-		ContentFormattingSteps.class, SimpleSteps.class, BasicEditorSteps.class })
-@BeforeAndAfterProcessors({ SWTBotInitialiser.class })
+@SubStepsConfiguration(featureFile = "features/sample.feature", stepImplementations = { ContentFormattingSteps.class,
+        SimpleSteps.class, BasicEditorSteps.class }, beforeAndAfterImplementations = { SWTBotInitialiser.class })
 public class SimpleTest {
 }
--- com.technophobia.substeps.editor.tests.acceptance/src/main/java/com/technophobia/substeps/editor/steps/EditorBeforeAndAfterExecutor.java
+++ com.technophobia.substeps.editor.tests.acceptance/src/main/java/com/technophobia/substeps/editor/steps/EditorBeforeAndAfterExecutor.java
@@ -2,15 +2,15 @@ package com.technophobia.substeps.editor.steps;
 
 import com.technophobia.substeps.editor.controller.FeatureFileEditorController;
 import com.technophobia.substeps.editor.controller.SWTControllerFactory;
-import com.technophobia.substeps.runner.JunitFeatureRunner.AfterEveryScenario;
+import com.technophobia.substeps.runner.setupteardown.Annotations.AfterEveryScenario;
 
 public class EditorBeforeAndAfterExecutor {
 
-	@AfterEveryScenario
-	public void closeAllOpenEditors() {
-		final FeatureFileEditorController controller = SWTControllerFactory
-				.createFor(FeatureFileEditorController.class);
+    @AfterEveryScenario
+    public void closeAllOpenEditors() {
+        final FeatureFileEditorController controller = SWTControllerFactory
+                .createFor(FeatureFileEditorController.class);
 
-		controller.closeAllOpenEditors();
-	}
+        controller.closeAllOpenEditors();
+    }
 }
