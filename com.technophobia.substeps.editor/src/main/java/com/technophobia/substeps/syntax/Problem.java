ef207e0ec3edf7433fa45367ad6115f050b3babc
Stu Forbes <mr.stu.forbes@gmail.com>
2012-11-22 14:23:30 +0000

    Fixes to problems to work with new offset calculations in core

--- com.technophobia.substeps.editor/src/main/java/com/technophobia/substeps/syntax/Problem.java
+++ com.technophobia.substeps.editor/src/main/java/com/technophobia/substeps/syntax/Problem.java
@@ -12,28 +12,28 @@ public class Problem {
     private final String description;
     private final String offendingLine;
     private final int lineNumber;
-    private final int offset;
+    private final int lineOffset;
 
 
     public static Problem createError(final String description, final String line, final int lineNumber,
-            final int offset) {
-        return new Problem(IMarker.SEVERITY_ERROR, description, line, lineNumber, offset);
+            final int lineOffset) {
+        return new Problem(IMarker.SEVERITY_ERROR, description, line, lineNumber, lineOffset);
     }
 
 
     public static Problem createWarning(final String description, final String line, final int lineNumber,
-            final int offset) {
-        return new Problem(IMarker.SEVERITY_WARNING, description, line, lineNumber, offset);
+            final int lineOffset) {
+        return new Problem(IMarker.SEVERITY_WARNING, description, line, lineNumber, lineOffset);
     }
 
 
     private Problem(final int severity, final String description, final String offendingLine, final int lineNumber,
-            final int offset) {
+            final int lineOffset) {
         this.severity = severity;
         this.description = description;
         this.offendingLine = offendingLine;
         this.lineNumber = lineNumber;
-        this.offset = offset;
+        this.lineOffset = lineOffset;
     }
 
 
@@ -44,8 +44,9 @@ public class Problem {
             marker.setAttribute(IMarker.MESSAGE, description);
             marker.setAttribute(IMarker.PRIORITY, IMarker.PRIORITY_HIGH);
             marker.setAttribute(IMarker.LINE_NUMBER, lineNumber + 1);
-            marker.setAttribute(IMarker.CHAR_START, offset);
-            marker.setAttribute(IMarker.CHAR_END, (offset + offendingLine.trim().length()));
+
+            marker.setAttribute(IMarker.CHAR_START, lineOffset);
+            marker.setAttribute(IMarker.CHAR_END, (lineOffset + offendingLine.trim().length()));
             return true;
         } catch (final CoreException ex) {
             FeatureEditorPlugin.instance().error("Could not mark resource " + resource, ex);
