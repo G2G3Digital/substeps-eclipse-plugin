28e30b598534c7b4b9f6cbd71383fd82335d431e
Stu Forbes <mr.stu.forbes@gmail.com>
2012-11-23 16:37:11 +0000

    Updating copyright & license information in projects/source files

--- com.technophobia.substeps.editor.tests.unit/src/com/technophobia/substeps/render/fake/FakeStepImplementationRendererTarget.java
+++ com.technophobia.substeps.editor.tests.unit/src/com/technophobia/substeps/render/fake/FakeStepImplementationRendererTarget.java
@@ -1,3 +1,19 @@
+/*******************************************************************************
+ * Copyright Technophobia Ltd 2012
+ * 
+ * This file is part of the Substeps Eclipse Plugin.
+ * 
+ * The Substeps Eclipse Plugin is free software: you can redistribute it and/or modify
+ * it under the terms of the Eclipse Public License v1.0.
+ * 
+ * The Substeps Eclipse Plugin is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Eclipse Public License for more details.
+ * 
+ * You should have received a copy of the Eclipse Public License
+ * along with the Substeps Eclipse Plugin.  If not, see <http://www.eclipse.org/legal/epl-v10.html>.
+ ******************************************************************************/
 package com.technophobia.substeps.render.fake;
 
 import com.technophobia.substeps.model.SubSteps.Step;
