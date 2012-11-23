df3e82a08a457863c20385d05d14dd64b3b9c2d0
Stu Forbes <mr.stu.forbes@gmail.com>
2012-11-23 16:00:41 +0000

    Removing previous Copyright text

--- com.technophobia.substeps.editor.tests.unit/src/com/technophobia/substeps/document/content/partition/ContentTypeRuleBasedPartitionScannerFactoryTest.java
+++ com.technophobia.substeps.editor.tests.unit/src/com/technophobia/substeps/document/content/partition/ContentTypeRuleBasedPartitionScannerFactoryTest.java
@@ -1,21 +1,3 @@
-/*
- *	Copyright Technophobia Ltd 2012
- *
- *   This file is part of Substeps.
- *
- *    Substeps is free software: you can redistribute it and/or modify
- *    it under the terms of the GNU Lesser General Public License as published by
- *    the Free Software Foundation, either version 3 of the License, or
- *    (at your option) any later version.
- *
- *    Substeps is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *    GNU Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public License
- *    along with Substeps.  If not, see <http://www.gnu.org/licenses/>.
- */
 package com.technophobia.substeps.document.content.partition;
 
 import static org.hamcrest.Matchers.hasItems;
@@ -36,44 +18,47 @@ import com.technophobia.substeps.document.content.ContentTypeDefinitionFactory;
 @RunWith(JMock.class)
 public class ContentTypeRuleBasedPartitionScannerFactoryTest {
 
-	private Mockery context;
+    private Mockery context;
 
-	private ContentTypeDefinitionFactory contentTypeDefinitionFactory;
+    private ContentTypeDefinitionFactory contentTypeDefinitionFactory;
 
-	private ContentTypeRuleBasedPartitionScannerFactory partitionScannerFactory;
+    private ContentTypeRuleBasedPartitionScannerFactory partitionScannerFactory;
 
-	@Before
-	public void initialiseFactory() {
-		this.context = new Mockery();
 
-		this.contentTypeDefinitionFactory = context.mock(ContentTypeDefinitionFactory.class);
+    @Before
+    public void initialiseFactory() {
+        this.context = new Mockery();
 
-		this.partitionScannerFactory = new ContentTypeRuleBasedPartitionScannerFactory(contentTypeDefinitionFactory);
-	}
+        this.contentTypeDefinitionFactory = context.mock(ContentTypeDefinitionFactory.class);
 
-	@Test
-	public void canCreateLegalContentTypesFromDefinitionIds() {
+        this.partitionScannerFactory = new ContentTypeRuleBasedPartitionScannerFactory(contentTypeDefinitionFactory);
+    }
 
-		final ContentTypeDefinition definition1 = context.mock(ContentTypeDefinition.class, "definition1");
-		final ContentTypeDefinition definition2 = context.mock(ContentTypeDefinition.class, "definition2");
-		final ContentTypeDefinition definition3 = context.mock(ContentTypeDefinition.class, "definition3");
 
-		context.checking(new Expectations() {
-			{
-				oneOf(definition1).id();
-				will(returnValue("definition-1"));
+    @Test
+    public void canCreateLegalContentTypesFromDefinitionIds() {
 
-				oneOf(definition2).id();
-				will(returnValue("definition-2"));
+        final ContentTypeDefinition definition1 = context.mock(ContentTypeDefinition.class, "definition1");
+        final ContentTypeDefinition definition2 = context.mock(ContentTypeDefinition.class, "definition2");
+        final ContentTypeDefinition definition3 = context.mock(ContentTypeDefinition.class, "definition3");
 
-				oneOf(definition3).id();
-				will(returnValue("definition-3"));
+        context.checking(new Expectations() {
+            {
+                oneOf(definition1).id();
+                will(returnValue("definition-1"));
 
-				oneOf(contentTypeDefinitionFactory).contentTypeDefinitions();
-				will(returnValue(new ContentTypeDefinition[] { definition1, definition2, definition3 }));
-			}
-		});
+                oneOf(definition2).id();
+                will(returnValue("definition-2"));
 
-		assertThat(Arrays.asList(partitionScannerFactory.legalContentTypes()), hasItems("definition-1", "definition-2", "definition-3"));
-	}
+                oneOf(definition3).id();
+                will(returnValue("definition-3"));
+
+                oneOf(contentTypeDefinitionFactory).contentTypeDefinitions();
+                will(returnValue(new ContentTypeDefinition[] { definition1, definition2, definition3 }));
+            }
+        });
+
+        assertThat(Arrays.asList(partitionScannerFactory.legalContentTypes()),
+                hasItems("definition-1", "definition-2", "definition-3"));
+    }
 }
--- com.technophobia.substeps.editor/src/main/java/com/technophobia/substeps/actions/HelloWorldAction.java
+++ com.technophobia.substeps.editor/src/main/java/com/technophobia/substeps/actions/HelloWorldAction.java
@@ -1,21 +1,3 @@
-/*
- *	Copyright Technophobia Ltd 2012
- *
- *   This file is part of Substeps.
- *
- *    Substeps is free software: you can redistribute it and/or modify
- *    it under the terms of the GNU Lesser General Public License as published by
- *    the Free Software Foundation, either version 3 of the License, or
- *    (at your option) any later version.
- *
- *    Substeps is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *    GNU Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public License
- *    along with Substeps.  If not, see <http://www.gnu.org/licenses/>.
- */
 package com.technophobia.substeps.actions;
 
 import org.eclipse.jface.action.IAction;
@@ -31,34 +13,37 @@ import org.eclipse.ui.IWorkbenchWindowActionDelegate;
  * "Hello World" message.
  */
 public class HelloWorldAction implements IWorkbenchWindowActionDelegate {
-	IWorkbenchWindow activeWindow = null;
+    IWorkbenchWindow activeWindow = null;
 
-	/**
-	 * Run the action. Display the Hello World message
-	 */
-	@Override
-	public void run(final IAction proxyAction) {
-		// proxyAction has UI information from manifest file (ignored)
-		final Shell shell = activeWindow.getShell();
-		MessageDialog.openInformation(shell, "Hello World", "Hello World!");
-	}
 
-	// IActionDelegate method
-	@Override
-	public void selectionChanged(final IAction proxyAction,
-			final ISelection selection) {
-		// do nothing, action is not dependent on the selection
-	}
+    /**
+     * Run the action. Display the Hello World message
+     */
+    @Override
+    public void run(final IAction proxyAction) {
+        // proxyAction has UI information from manifest file (ignored)
+        final Shell shell = activeWindow.getShell();
+        MessageDialog.openInformation(shell, "Hello World", "Hello World!");
+    }
 
-	// IWorkbenchWindowActionDelegate method
-	@Override
-	public void init(final IWorkbenchWindow window) {
-		activeWindow = window;
-	}
 
-	// IWorkbenchWindowActionDelegate method
-	@Override
-	public void dispose() {
-		// nothing to do
-	}
+    // IActionDelegate method
+    @Override
+    public void selectionChanged(final IAction proxyAction, final ISelection selection) {
+        // do nothing, action is not dependent on the selection
+    }
+
+
+    // IWorkbenchWindowActionDelegate method
+    @Override
+    public void init(final IWorkbenchWindow window) {
+        activeWindow = window;
+    }
+
+
+    // IWorkbenchWindowActionDelegate method
+    @Override
+    public void dispose() {
+        // nothing to do
+    }
 }
