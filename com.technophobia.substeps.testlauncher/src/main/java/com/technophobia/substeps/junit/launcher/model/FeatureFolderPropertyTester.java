46e033d790fccc4ec8c465dd303689816cbba1ba
Stu Forbes <mr.stu.forbes@gmail.com>
2013-07-03 08:33:38 +0100

    Feature folders can now be executed

--- com.technophobia.substeps.testlauncher/src/main/java/com/technophobia/substeps/junit/launcher/model/FeatureFolderPropertyTester.java
+++ com.technophobia.substeps.testlauncher/src/main/java/com/technophobia/substeps/junit/launcher/model/FeatureFolderPropertyTester.java
@@ -1,37 +1,34 @@
 package com.technophobia.substeps.junit.launcher.model;
 
 import org.eclipse.core.expressions.PropertyTester;
-import org.eclipse.core.resources.IContainer;
-import org.eclipse.core.resources.IProject;
-import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.resources.IFolder;
 
 import com.technophobia.substeps.FeatureEditorPlugin;
+import com.technophobia.substeps.predicate.IsFeatureFolderPredicate;
+import com.technophobia.substeps.supplier.Predicate;
 
 public class FeatureFolderPropertyTester extends PropertyTester {
 
     private static final String PROPERTY_NAME = "isFeatureFolder";
 
+    private final Predicate<IFolder> predicate;
+
+
+    public FeatureFolderPropertyTester() {
+        this(new IsFeatureFolderPredicate(FeatureEditorPlugin.instance().projectManager()));
+    }
+
+
+    public FeatureFolderPropertyTester(final Predicate<IFolder> predicate) {
+        this.predicate = predicate;
+    }
+
 
     @Override
     public boolean test(final Object receiver, final String property, final Object[] args, final Object expectedValue) {
         if (PROPERTY_NAME.equals(property)) {
-            final IContainer folder = (IContainer) receiver;
-            final IProject project = folder.getProject();
-            final IPath featureFolderPath = featureFolderForProject(project);
-
-            return isDescendantOf(folder.getFullPath(), featureFolderPath);
+            return predicate.forModel((IFolder) receiver);
         }
         return false;
     }
-
-
-    private boolean isDescendantOf(final IPath folder, final IPath featureFolderPath) {
-        return featureFolderPath.isPrefixOf(folder);
-    }
-
-
-    private IPath featureFolderForProject(final IProject project) {
-        return FeatureEditorPlugin.instance().projectManager().featureFolderFor(project);
-    }
-
 }
